6250f3f6deb04cafc89b2066345d66cf0d72a637
ChrCoello <christopher.coello@gmail.com>
2017-10-31 18:46:28 +0100

    intensity and landmarks

--- 2D/landmarks/runApplyTransform.sh
+++ 2D/landmarks/runApplyTransform.sh
@@ -3,47 +3,109 @@
 ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=4  # controls multi-threading
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS
 dim=2
-antsApplyTransforms -v -d $dim \
-	-i moving.png -r target.png -n linear \
-	-t moving2target0GenericAffine.mat \
-	-o moving_affine_onlypixel.nii.gz
 
-ImageMath 2 moving_affine_onlypixel.png Byte moving_affine_onlypixel.nii.gz
+
+# Only intensity
 
 antsApplyTransforms -v -d $dim \
 	-i moving.png -r target.png -n linear \
-	-t moving2target0GenericAffine.mat \
-	-t moving2target1Warp.nii.gz \
-	-o moving_warp_onlypixel.nii.gz
-
-ImageMath 2 moving_warp_onlypixel.png Byte moving_warp_onlypixel.nii.gz
+	-t moving2target_land0pix100GenericAffine.mat \
+	-o moving_affine_land0pix10.nii.gz
+ImageMath 2 moving_affine_land0pix10.png Byte moving_affine_land0pix10.nii.gz
 
 antsApplyTransforms -v -d $dim \
 	-i moving.png -r target.png -n linear \
-	-t moving2target_landmarks0GenericAffine.mat \
-	-o moving_affine_onlylandmarks.nii.gz
+	-t moving2target_land0pix100GenericAffine.mat \
+	-t moving2target_land0pix101Warp.nii.gz \
+	-o moving_warp_land0pix10.nii.gz
+ImageMath 2 moving_warp_land0pix10.png Byte moving_warp_land0pix10.nii.gz
 
-ImageMath 2 moving_affine_onlylandmarks.png Byte moving_affine_onlylandmarks.nii.gz
+convert moving_affine_land0pix10.png -background black -channel green -combine moving_affine_land0pix10_green.png
+composite -blend 50 -gravity South moving_affine_land0pix10_green.png target.png land_int_blend_001.png
 
-antsApplyTransforms -v -d $dim \
-	-i moving.png -r target.png -n linear \
-	-t moving2target_landmarks0GenericAffine.mat \
-	-t moving2target_landmarks1Warp.nii.gz \
-	-o moving_warp_onlylandmarks.nii.gz
 
-ImageMath 2 moving_warp_onlylandmarks.png Byte moving_warp_onlylandmarks.nii.gz
+# Landmarks 0.2 and Intensity 0.8
 
 antsApplyTransforms -v -d $dim \
 	-i moving.png -r target.png -n linear \
 	-t moving2target_land2pix80GenericAffine.mat \
 	-o moving_affine_land2pix8.nii.gz
-
 ImageMath 2 moving_affine_land2pix8.png Byte moving_affine_land2pix8.nii.gz
 
 antsApplyTransforms -v -d $dim \
 	-i moving.png -r target.png -n linear \
 	-t moving2target_land2pix80GenericAffine.mat \
 	-t moving2target_land2pix81Warp.nii.gz \
-	-o moving_warp_onlylandmarks.nii.gz
-
+	-o moving_warp_land2pix8.nii.gz
 ImageMath 2 moving_warp_land2pix8.png Byte moving_warp_land2pix8.nii.gz
+
+convert moving_affine_land2pix8.png -background black -channel green -combine moving_affine_land2pix8_green.png
+composite -blend 50 -gravity South moving_affine_land2pix8_green.png target.png land_int_blend_002.png
+
+
+# Landmarks 0.5 and Intensity 0.5
+
+antsApplyTransforms -v -d $dim \
+	-i moving.png -r target.png -n linear \
+	-t moving2target_land5pix50GenericAffine.mat \
+	-o moving_affine_land5pix5.nii.gz
+ImageMath 2 moving_affine_land5pix5.png Byte moving_affine_land5pix5.nii.gz
+
+antsApplyTransforms -v -d $dim \
+	-i moving.png -r target.png -n linear \
+	-t moving2target_land5pix50GenericAffine.mat \
+	-t moving2target_land5pix51Warp.nii.gz \
+	-o moving_warp_land5pix5.nii.gz
+ImageMath 2 moving_warp_land5pix5.png Byte moving_warp_land5pix5.nii.gz
+
+convert moving_affine_land5pix5.png -background black -channel green -combine moving_affine_land5pix5_green.png
+composite -blend 50 -gravity South moving_affine_land5pix5_green.png target.png land_int_blend_003.png
+
+# Landmarks 0.8 and Intensity 0.2
+
+antsApplyTransforms -v -d $dim \
+	-i moving.png -r target.png -n linear \
+	-t moving2target_land8pix20GenericAffine.mat \
+	-o moving_affine_land8pix2.nii.gz
+ImageMath 2 moving_affine_land8pix2.png Byte moving_affine_land8pix2.nii.gz
+
+antsApplyTransforms -v -d $dim \
+	-i moving.png -r target.png -n linear \
+	-t moving2target_land8pix20GenericAffine.mat \
+	-t moving2target_land8pix21Warp.nii.gz \
+	-o moving_warp_land8pix2.nii.gz
+ImageMath 2 moving_warp_land8pix2.png Byte moving_warp_land8pix2.nii.gz
+
+convert moving_affine_land8pix2.png -background black -channel green -combine moving_affine_land8pix2_green.png
+composite -blend 50 -gravity South moving_affine_land8pix2_green.png target.png land_int_blend_004.png
+
+
+# Only landmarks
+
+antsApplyTransforms -v -d $dim \
+	-i moving.png -r target.png -n linear \
+	-t moving2target_land10pix00GenericAffine.mat \
+	-o moving_affine_land10pix0.nii.gz
+ImageMath 2 moving_affine_land10pix0.png Byte moving_affine_land10pix0.nii.gz
+
+antsApplyTransforms -v -d $dim \
+	-i moving.png -r target.png -n linear \
+	-t moving2target_land10pix00GenericAffine.mat \
+	-t moving2target_land10pix01Warp.nii.gz \
+	-o moving_warp_land10pix0.nii.gz
+ImageMath 2 moving_warp_land10pix0.png Byte moving_warp_land10pix0.nii.gz
+
+convert moving_affine_land10pix0.png -background black -channel green -combine moving_affine_land10pix0_green.png
+composite -blend 50 -gravity South moving_affine_land10pix0_green.png target.png land_int_blend_005.png
+
+# original
+cp moving.png land_int_blend_000.png
+
+# combine in a gif
+convert -delay 50 -loop 0 land_int_blend_00*.png intensity2landmarks.gif
+
+
+
+
+
+
--- 2D/landmarks/runElasticLandmarks.sh
+++ 2D/landmarks/runElasticLandmarks.sh
@@ -3,15 +3,84 @@
 ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS=4  # controls multi-threading
 export ITK_GLOBAL_DEFAULT_NUMBER_OF_THREADS
 dim=2
-antsRegistration -v -d $dim -r [ target.nii.gz , moving.nii.gz ,1] \
-      -m mattes[ target.nii.gz , moving.nii.gz, 1 , 32, regular,0.1 ] \
-      -t affine[ 0.1 ] \
-      -c [500x500x50,1.e-8,20] \
-      -s 4x2x1vox \
-      -f 3x2x1 -l 1 \
-      -m cc[ target.nii.gz , moving.nii.gz, 1 , 4 ] \
-      -t BSplineSyN[ .15, 8, 0.5 ] \
-      -c [ 50x50x50,0,5 ] \
-      -s 1x0.5x0vox \
-      -f 4x2x1 -l 1 -u 1 -z 1 \
-      -o [moving,moving_diff.nii.gz,moving_inv.nii.gz]
+
+# Only intensity
+antsRegistration -v -d $dim -r [ target.png , moving.png ,1] \
+	-m pse[ surfaceTarget.nii.gz , surfaceMoving.nii.gz, 0,1,0,1,20] \
+      	-m mattes[ target.png , moving.png, 1 , 32, regular,0.1 ] \
+      	-t affine[ 0.1 ] \
+      	-c [500,1.e-7,20] \
+      	-s 4vox \
+      	-f 4 -l 1 \
+      	-m pse[surfaceTarget.nii.gz , surfaceMoving.nii.gz, 0,1,0,1,20] \
+	-m cc[target.png , moving.png, 1, 4] \
+      	-t BSplineSyN[ .15, 8, 0.5 ] \
+      	-c [50,1.e-7,5 ] \
+      	-s 4vox \
+      	-f 4 -u 1 -z 1 \
+      	-o moving2target_land0pix10
+
+# Landmarks 0.2 and Intensity 0.8
+antsRegistration -v -d $dim -r [ target.png , moving.png ,1] \
+	-m pse[ surfaceTarget.nii.gz , surfaceMoving.nii.gz, 0.2,1,0,1,20] \
+      	-m mattes[ target.png , moving.png, 0.8 , 32, regular,0.1 ] \
+      	-t affine[ 0.1 ] \
+      	-c [500,1.e-7,20] \
+      	-s 4vox \
+      	-f 4 -l 1 \
+      	-m pse[surfaceTarget.nii.gz , surfaceMoving.nii.gz, 0.2,1,0,1,20] \
+	-m cc[target.png , moving.png, 0.8, 4] \
+      	-t BSplineSyN[ .15, 8, 0.5 ] \
+      	-c [50,1.e-7,5 ] \
+      	-s 4vox \
+      	-f 4 -u 1 -z 1 \
+      	-o moving2target_land2pix8
+
+# Landmarks 0.5 and Intensity 0.5
+antsRegistration -v -d $dim -r [ target.png , moving.png ,1] \
+	-m pse[ surfaceTarget.nii.gz , surfaceMoving.nii.gz, 0.5,1,0,1,20] \
+      	-m mattes[ target.png , moving.png, 0.5 , 32, regular,0.1 ] \
+      	-t affine[ 0.1 ] \
+      	-c [500,1.e-7,20] \
+      	-s 4vox \
+      	-f 4 -l 1 \
+      	-m pse[surfaceTarget.nii.gz , surfaceMoving.nii.gz, 0.5,1,0,1,20] \
+	-m cc[target.png , moving.png, 0.5, 4] \
+      	-t BSplineSyN[ .15, 8, 0.5 ] \
+      	-c [50,1.e-7,5 ] \
+      	-s 4vox \
+      	-f 4 -u 1 -z 1 \
+      	-o moving2target_land5pix5
+
+# Landmarks 0.8 and Intensity 0.2
+antsRegistration -v -d $dim -r [ target.png , moving.png ,1] \
+	-m pse[ surfaceTarget.nii.gz , surfaceMoving.nii.gz, 0.8,1,0,1,20] \
+      	-m mattes[ target.png , moving.png, 0.2 , 32, regular,0.1 ] \
+      	-t affine[ 0.1 ] \
+      	-c [500,1.e-7,20] \
+      	-s 4vox \
+      	-f 4 -l 1 \
+      	-m pse[surfaceTarget.nii.gz , surfaceMoving.nii.gz, 0.8,1,0,1,20] \
+	-m cc[target.png , moving.png, 0.2, 4] \
+      	-t BSplineSyN[ .15, 8, 0.5 ] \
+      	-c [50,1.e-7,5 ] \
+      	-s 4vox \
+      	-f 4 -u 1 -z 1 \
+      	-o moving2target_land8pix2
+
+
+# Only landmarks
+antsRegistration -v -d $dim -r [ target.png , moving.png ,1] \
+	-m pse[ surfaceTarget.nii.gz , surfaceMoving.nii.gz, 1,1,0,1,20] \
+      	-m mattes[ target.png , moving.png, 0 , 32, regular,0.1 ] \
+      	-t affine[ 0.1 ] \
+      	-c [500,1.e-7,20] \
+      	-s 4vox \
+      	-f 4 -l 1 \
+      	-m pse[surfaceTarget.nii.gz , surfaceMoving.nii.gz, 1,1,0,1,20] \
+	-m cc[target.png , moving.png, 0, 4] \
+      	-t BSplineSyN[ .15, 8, 0.5 ] \
+      	-c [50,1.e-7,5 ] \
+      	-s 4vox \
+      	-f 4 -u 1 -z 1 \
+      	-o moving2target_land10pix0
